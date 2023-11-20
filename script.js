e4f078e227ac5c495bc2966ddc03fcfdf619bad1
ddlpr <103121607+ddlpr@users.noreply.github.com>
2023-11-20 14:50:42 -0500

    Strike through items on click
    Add delete item icon. Clean up styles.

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
+'use strict';
 // State variables & elements |>
 const button = document.getElementById("agregar");
-const addFromStorage = function(task) {
-  let lista = document.getElementById('lista');
-  let li = document.createElement('li');
-  li.textContent = task;
-  li.onclick = function() {
-    li.classList.toggle('tachado');
-  }
+const lista = document.getElementById('lista');
 
-  lista.append(li);
-}
+let tasks = [];
+const addFromStorage = function(task) {
+  const liElement = document.createElement('li');
+  liElement.classList.add('list-group-item', 'd-flex', 'justify-content-between');
+
+  const spanElement = document.createElement('span');
+  spanElement.textContent = task.description;
+
+  const iElement = document.createElement('i');
+  iElement.classList.add('bi', 'bi-trash3-fill');
+
+  if (task.completed) spanElement.classList.add('completed');
+
+  liElement.append(spanElement, iElement);
+  lista.append(liElement);
+};
 // State variables & elements <|
 
 const addTask = () => {
-  let task = document.getElementById('task').value;
-  if (!task) {
+  //let task = document.getElementById('task').value;
+  const task = {
+    description: document.getElementById('task').value,
+    completed: false
+  };
+  if (!task.description) {
     alert("Agregar tarea");
   } else {
     tasks.push(task);
+    const liElement = document.createElement('li');
+    liElement.classList.add('list-group-item', 'd-flex', 'justify-content-between');
+    const spanElement = document.createElement('span');
+    spanElement.textContent = task.description;
+    const iElement = document.createElement('i');
+    iElement.classList.add('bi', 'bi-trash3-fill');
 
-    let lista = document.getElementById('lista');
-    let li = document.createElement('li');
-    li.textContent = task;
-    li.onclick = function() {
-      li.classList.toggle('tachado');
-    }
+    liElement.append(spanElement, iElement);
+    lista.append(liElement);
 
-    lista.append(li);
     document.getElementById('task').value = '';
     localStorage.setItem('tareas', JSON.stringify(tasks));
-    console.log(localStorage);
   }
 }
 
-let tasks = [];
+lista.addEventListener('click', function(e) {
+  console.log(tasks);
+  if (e.target.tagName === 'LI') {
+    e.target.querySelector('span').classList.toggle('completed');
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentNode.querySelector('span').classList.toggle('completed');
+  }
+})
+
 
 document.addEventListener('DOMContentLoaded', function() {
   if (localStorage.getItem('tareas')) {
     tasks = JSON.parse(localStorage.tareas);
-    tasks.forEach(function (task) {
+    tasks.forEach(function(task) {
       addFromStorage(task);
     });
   }
 });
 
 button.onclick = addTask;
-localStorage.removeItem('tareas');
+//localStorage.removeItem('tareas');
